bfb8b244b841509cb243cdae974663b31f8d6d1a
2632151465 <2632151465@qq.com>
2023-05-08 10:43:58 +0800

    更新历史更新记录

--- 爱玩机/Busybox_for_Android_NDK_New/install_utils.sh
+++ 爱玩机/Busybox_for_Android_NDK_New/install_utils.sh
@@ -9,7 +9,7 @@ else
   filesDir=/data/user/$userId/${pkgName}/files
 fi
 installDir=$filesDir/busybox
-installCacheDir=$installDir/busybox/cache
+installCacheDir=$installDir/cache
 
 abi=$(getprop ro.product.cpu.abi)
 
